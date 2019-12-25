f0e01db739d1db5246ee28ecab64f271e5c16c1f
Kirito-se7en <695804696@qq.com>
2019-12-25 10:45:13 +0800

    Jianfeng Wu 2019-12-25 -Initialize

---
+++ src/test/java/com/se7en/bilibili/BilibiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se7en.bilibili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilibiliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
