d7c891cb159e30d121a74291b224ed9242a19d60
Kirito-JianfengWu <695804696@qq.com>
2020-06-28 15:14:48 +0800

    Jianfeng Wu 2020-06-28 - Update

--- src/main/java/com/se7en/bilibili/service/AuthoritiesRoleService.java
+++ src/main/java/com/se7en/bilibili/service/AuthoritiesRoleService.java
@@ -1,21 +1,15 @@
 package com.se7en.bilibili.service;
 
-import com.se7en.bilibili.repository.AuthoritiesRoleRepository;
-import com.se7en.bilibili.entity.Result;
-import com.hhsj.base.BaseService;
 import com.se7en.bilibili.entity.AuthoritiesRole;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-
-public interface AuthoritiesRoleService extends BaseService<AuthoritiesRole, String>  {
+public class AuthoritiesRoleService {
 
    //根据id查询
-   public Result getOne(String id);
+   public AuthoritiesRole getOne(String id);
 
    //根据id删除
-   public Result deleteOne(String id);
+   public AuthoritiesRole deleteOne(String id);
 
    //根据id保存
-   public Result saveOne(AuthoritiesRole entity);
+   public AuthoritiesRole saveOne(AuthoritiesRole entity);
 }
